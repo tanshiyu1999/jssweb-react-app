9f1a0c18f43e40c6e0d8930ac1a861667a1ebccd
tanshiyu <tanshiyu1999@gmail.com>
2023-06-13 21:31:11 +0800

    The last instane of the old event carousel

--- client/src/components/events/EventCarousel.jsx
+++ client/src/components/events/EventCarousel.jsx
@@ -57,13 +57,14 @@ function EventCarousel() {
     setIsHover(false);
  };
 
-
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+  
   const goToNext = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
