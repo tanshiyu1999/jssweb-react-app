9ec60f1272faa4ccb006f67410f87e6bafca7124
tanshiyu <tanshiyu1999@gmail.com>
2023-06-14 01:15:06 +0800

    Event Carousel Done

--- client/src/components/events/EventCarousel.jsx
+++ client/src/components/events/EventCarousel.jsx
@@ -1,109 +1,91 @@
-// Can use HTML ENTITY FOR PICS
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './EventCarousel.css'
+
+const EventCarousel = () => {
+
+  const [ counter, setCounter ] = useState(1);
+
+  // useEffect(() => {
+
+  //   const interval = setInterval(() => {      
+  //     if (counter > 4) {
+  //       setCounter(1);
+  //     }
+  //     document.getElementById('radio' + counter).checked = true;
+  //     setCounter(counter + 1);
+  //     if (counter > 4) {
+  //       setCounter(1);
+  //     }
+  //   }, 7000)
+
+  //   return () => clearInterval(interval)
+  // })
 
 
-function EventCarousel() {
-  const sliderStyles = {
-    position: "relative",
-    height: "100%",
-  };
-  
-  const dotsContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
-  };
-  
-  const dotStyle = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "20px",
-  };
-  
-  const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "10px",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    display: "block",
-  };
-  
-  const overlay = {
-    position: "absolute",
-    bottom: "0",
-    left: "0",
-    right: "0",
-    backgroundColor: "black",
-    opacity: "0.76",
-    overflow: "hidden",
-    color: "white",
-    width: "100%",
-    transition: ".5s ease",
-  }
 
-  const slides = [
-    { url: "http://placekitten.com/700/500", title: "beach" },
-    { url: "http://placekitten.com/500/300", title: "boat" },
-    { url: "http://placekitten.com/200/300", title: "forest" },
-    { url: "http://placekitten.com/400/500", title: "italy" },
-  ]
-
-  const [ isHover, setIsHover ] = useState(false);
-  const handleMouseEnter = () => {
-    setIsHover(true);
-  };
-
- const handleMouseLeave = () => {
-    setIsHover(false);
- };
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-  const slideStylesWidthBackground = {
-    ...slideStyles,
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  };
 
   return (
-    <div 
-      style={sliderStyles}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div style={slideStylesWidthBackground}>
-      <div style={overlay}>
-        <h3>JCF</h3>
-        <p>JCF Description</p>
-      </div>
-      </div>
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div
-            style={dotStyle}
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-          >
-            ●
+    // image slider start
+    <div className="slider">
+      <div className="slides">
+        {/* radio buttons start */}
+        <input type="radio" name="radio-btn" id="radio1" onClick={() => setCounter(1)} />
+        <input type="radio" name="radio-btn" id="radio2" onClick={() => setCounter(2)} />
+        <input type="radio" name="radio-btn" id="radio3" onClick={() => setCounter(3)} />
+        <input type="radio" name="radio-btn" id="radio4" onClick={() => setCounter(4)} />
+        {/* radio buttons end */}
+        {/* slide images start */}
+        <div className="slide first">
+          <img src="http://placekitten.com/200/300" alt="" />
+          <div className="text-container">
+            <h3 className="text-title">JCF</h3>
+            <p className="text-description">Fun and exciting events happening every month including sharing sessions, workshops, Christmas Party, etc. Keep up to date with our social channels!</p>
           </div>
-        ))}
+        </div>
+        <div className="slide">
+          <img src="http://placekitten.com/200/300" alt="" />
+          <div className="text-container">
+            <h3 className="text-title">JCF</h3>
+            <p className="text-description">Fun and exciting events happening every month including sharing sessions, workshops, Christmas Party, etc. Keep up to date with our social channels!</p>
+          </div>
+        </div>
+        <div className="slide">
+          <img src="http://placekitten.com/200/300" alt="" />
+          <div className="text-container">
+            <h3 className="text-title">JCF</h3>
+            <p className="text-description">Fun and exciting events happening every month including sharing sessions, workshops, Christmas Party, etc. Keep up to date with our social channels!</p>
+          </div>
+        </div>
+        <div className="slide">
+          <img src="http://placekitten.com/200/300" alt="" />
+          <div className="text-container">
+            <h3 className="text-title">JCF</h3>
+            <p className="text-description">Fun and exciting events happening every month including sharing sessions, workshops, Christmas Party, etc. Keep up to date with our social channels!</p>
+          </div>
+        </div>
+        {/* slide images end */}
+
+        {/* automatic navigation start */}
+        <div className="navigation-auto">
+          <div className="auto-btn1"></div>
+          <div className="auto-btn2"></div>
+          <div className="auto-btn3"></div>
+          <div className="auto-btn4"></div>
+        </div>
+        {/* automatic navigation end */}
+
+        {/* manual navigation start */}
+        <div className="navigation-manual">
+          <label htmlFor="radio1" className="manual-btn"></label>
+          <label htmlFor="radio2" className="manual-btn"></label>
+          <label htmlFor="radio3" className="manual-btn"></label>
+          <label htmlFor="radio4" className="manual-btn"></label>
+        </div>
+        {/* manual navigation end */}
       </div>
     </div>
+    // image slider end
   )
 }
 
-
-export default EventCarousel;
+export default EventCarousel
