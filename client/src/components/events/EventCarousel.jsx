f331c022df819702614d5a8ff6bd9a70b44bde0f
tanshiyu <tanshiyu1999@gmail.com>
2023-06-13 16:26:33 +0800

    A instance of the EventCarousel Hardcoded

--- client/src/components/events/EventCarousel.jsx
+++ client/src/components/events/EventCarousel.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
-import Carousel from 'react-material-ui-carousel'
-import {Paper, Button} from '@mui/material'
+// Can use HTML ENTITY FOR PICS
+import React, { useState } from 'react'
 
-function Item(props) {
-  return (
-    <Paper>
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
 
-      <Button className="CheckButton">
-        Check it out!
-      </Button>
-    </Paper>
-  )
-}
 
-function EventCarousel(props) {
-  var items = [
-    {
-      name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!"
-    }, {
-      name: "Random Name #2",
-      description: "Hello World!"
-    }
+
+
+function EventCarousel() {
+
+  const slides = [
+    { url: "http://placekitten.com/700/500", title: "beach" },
+    { url: "http://placekitten.com/500/300", title: "boat" },
+    { url: "http://placekitten.com/200/300", title: "forest" },
+    { url: "http://placekitten.com/400/500", title: "italy" },
   ]
 
-  const output = items.map((item, i) => {
-    return <Item key={i} item={item}/>
-  })
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const slideStylesWidthBackground = {
+    ...slideStyles,
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
 
   return (
-    <Carousel
-      sx={{
-        width: '600px',
-      }}
-    > {output} </Carousel>
+    <div style={sliderStyles}>
+      <div style={slideStylesWidthBackground}>
+      <div style={textContainer}>
+        <h3>JCF</h3>
+        <p>JCF Description</p>
+      </div>
+      </div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <div
+            style={dotStyle}
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ●
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
 
 export default EventCarousel;
+
